d1bd09050cb794656a1cf30f1daf4563a3f3f2ca
smcro <smcro@DESKTOP-3U3QOFQ>
2022-06-12 18:06:17 -0700

    Add project files.

---
+++ Final/ILedger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crownhart_CashFlow
+{
+    internal interface ILedger
+    {
+        public enum LedgerType
+        {
+            Salaried,
+            Hourly,
+            Invoice
+        }
+    }
+}
